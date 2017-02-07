9f4d53779327d5f224507398fe9a5edf1238b7da
Tom Krepp <tommykrepp@aol.com>
2017-02-07 14:21:36 -0700

    Dashboard-Complete

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,46 @@
+// Dashboard JS
+// Moment!
 
-    $(function () {
-        $('#datetimepicker10').datetimepicker({
-            viewMode: 'years',
-            format: 'MM/YYYY'
-        });
+$(function () {
+    $('#datetimepicker10').datetimepicker({
+        viewMode: 'years',
+        format: 'MM/DD/YYYY'
     });
+});
+
+//age verification & beer-temp redirect!
+
+$(".beerBtn").on("click", function(event) {
+  event.preventDefault();
+
+  var date = $(".date-birth").val();
+
+  var birthdate = moment(date, "MM-DD-YYYY");
+
+  var rightNow = moment();
+
+  if(rightNow.diff(birthdate, 'years') > 21) {
+    window.location.replace("./beer-temp.html");
+  } else {
+    alert("Go Away!");
+  }
+});
+
+//age verification & wine-temp redirect!
+
+
+$(".wineBtn").on("click", function(event) {
+  event.preventDefault();
+
+  var date = $(".date-birth").val();
+
+  var birthdate = moment(date, "MM-DD-YYYY");
+
+  var rightNow = moment();
+
+  if(rightNow.diff(birthdate, 'years') > 21) {
+    window.location.replace("./wine-temp.html");
+  } else {
+    alert("Go Away!");
+  }
+});
