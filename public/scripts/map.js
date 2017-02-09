5ca4ec8aa45692586d451b6bc39ea74b6269fbff
Tom Krepp <tommykrepp@aol.com>
2017-02-09 11:57:40 -0700

    AJAXREQUEST

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,29 +1,28 @@
 
 
-$.ajax({
-    url:"http://api.brewerydb.com/v2/?key=c7522aeef54280ac173d168a0e08d9bb&output=json/locations",
-    method: 'GET',
-    dataType: 'json'
-  }).done(function(response){
-    console.log(response);
-  }).fail(function(err){
-    console.log("fail");
-    console.log(err);
-  });
+
+//
+// $.ajax({
+//     url:"http://api.brewerydb.com/v2/?key=c7522aeef54280ac173d168a0e08d9bb/locations",
+//     method: 'GET',
+//     dataType: 'json'
+//   }).done(function(response){
+//     console.log(response);
+//   }).fail(function(err){
+//     console.log("fail");
+//     console.log(err);
+//   });
 
 initMap();
-getPoints();
-toggleHeatmap();
-changeGradient();
-changeRadius();
-changeOpacity();
+
 
 ////////////// Map + Geolocation \\\\\\\\\\\\\\\
-
-var map, heatmap;
+var map;
+var heatmap;
+var pos;
 
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 37.775, lng: -122.434},
     zoom: 13
   });
@@ -37,25 +36,55 @@ function initMap() {
 
   // Try HTML5 geolocation.
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-      console.log(pos);
-      // infoWindow.setPosition(pos);
-      // infoWindow.setContent('Location found.');
-      map.setCenter(pos);
-    }, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
-    });
-
-
+    gps(infoWindow);
+    getPoints();
+    toggleHeatmap();
+    changeGradient();
+    changeRadius();
+    changeOpacity();
+    console.log(pos, 'outside of the function');
   } else {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
   }
 }
+
+function gps(infoWindow){
+  navigator.geolocation.getCurrentPosition(function(position) {
+    pos = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    };
+    console.log("gps");
+
+    var url = "http://galvanize-cors-proxy.herokuapp.com/https://api.brewerydb.com/v2/search/geo/point?lat="+pos.lat.toString()+"&lng="+pos.lng.toString()+"&key=c7522aeef54280ac173d168a0e08d9bb"
+    console.log(url);
+
+    $.ajax({
+        url: url,
+        // /search/geo/point?lat=35.772096&lng=-78.638614
+        method: 'GET',
+        dataType: 'json'
+      }).done(function(response){
+        console.log(response);
+      }).fail(function(err){
+        console.log("fail");
+        console.log(err);
+      });
+
+    console.log(pos, 'inside the function');
+    infoWindow.setPosition(pos);
+    infoWindow.setContent('Location found.');
+    map.setCenter(pos);
+    }, function() {
+    handleLocationError(true, infoWindow, map.getCenter());
+  });
+}
+
+
+
+console.log("something");
+
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
@@ -100,7 +129,12 @@ function changeOpacity() {
 }
 
 ////////////// Sets Points to Generate Heatmap \\\\\\\\\\\\\\\
+function getLatLng() {
 
-function getPoints() {
-  return [   ];
 }
+function getPoints() {
+  return [new google.maps.LatLng(39.756297 , -104.991249)];
+}
+
+
+////////Search\\\\\\\\\
